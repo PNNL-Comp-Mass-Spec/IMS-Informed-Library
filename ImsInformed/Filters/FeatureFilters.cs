9e011f61f64d9f06c25d57b3afe311d49cb125b3
Jian <jian.ma@pnnl.gov>
2015-02-13 00:29:40 +0000

    Missing files
    git-svn-id: https://prismsvn.pnl.gov/svn/User_Applications/Crowell_Utilities/ImsInformed/trunk@19437 91f24f89-0e13-0410-88a5-a0bb1538c76e

---
+++ ImsInformed/Filters/FeatureFilters.cs
@@ -0,0 +1,85 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FeatureFilters.cs" company="PNNL">
+//   Written for the Department of Energy (PNNL, Richland, WA)
+//   Copyright 2014, Battelle Memorial Institute.  All Rights Reserved.
+// </copyright>
+// <summary>
+//   Defines the FeatureFilters type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImsInformed.Filters
+{
+    using System;
+
+    using ImsInformed.Scoring;
+
+    using MultiDimensionalPeakFinding.PeakDetection;
+
+    /// <summary>
+    /// The feature filters.
+    /// </summary>
+    public class FeatureFilters
+    {
+        /// <summary>
+        /// The filter extreme drift time.
+        /// </summary>
+        /// <param name="feature">
+        /// The feature.
+        /// </param>
+        /// <param name="totalImsScans">
+        /// The total ims scans.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public static bool FilterExtremeDriftTime(FeatureBlob feature, int totalImsScans)
+        {
+            int scanImsRep = feature.Statistics.ScanImsRep;
+
+            // Nullify the intensity score if the Scan is in 1% scans left or right areas.
+            int errorMargin = (int)Math.Round(totalImsScans * 0.01);
+            return scanImsRep < errorMargin || scanImsRep > totalImsScans - errorMargin;
+        }
+
+        /// <summary>
+        /// The filter low intensity.
+        /// </summary>
+        /// <param name="feature">
+        /// The feature.
+        /// </param>
+        /// <param name="intensityScore">
+        /// The intensity score.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public static bool FilterLowIntensity(FeatureBlob feature, double intensityScore)
+        {
+            return intensityScore < 0.5;
+        }
+
+        public static bool FilterBadPeakShape(FeatureBlob feature, double peakShapeScore)
+        {
+            return peakShapeScore < 0.4;
+        }
+
+
+        /// <summary>
+        /// The filter bad isotopic profile.
+        /// </summary>
+        /// <param name="feature">
+        /// The feature.
+        /// </param>
+        /// <param name="totalImsScans">
+        /// The total ims scans.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public static bool FilterBadIsotopicProfile(FeatureBlob feature, double isotopicScore)
+        {
+            return isotopicScore < 0.4;
+        }
+    }
+}

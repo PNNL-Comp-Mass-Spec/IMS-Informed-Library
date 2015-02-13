0f8dd507dc7c0772d696f6f641ee610008d59082
Jian <jian.ma@pnnl.gov>
2015-02-13 02:06:51 +0000

    Added missing files....again.
    git-svn-id: https://prismsvn.pnl.gov/svn/User_Applications/Crowell_Utilities/ImsInformed/trunk@19440 91f24f89-0e13-0410-88a5-a0bb1538c76e

---
+++ ImsInformed/Scoring/TargetPresenceLikelihoodFunctions.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FeatureLikelihoodFunctions.cs" company="PNNL">
+//   Written for the Department of Energy (PNNL, Richland, WA)
+//   Copyright 2014, Battelle Memorial Institute.  All Rights Reserved.
+// </copyright>
+// <summary>
+//   Defines the FeatureLikelihoodFunctions type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImsInformed.Scoring
+{
+    using System;
+
+    /// <summary>
+    /// The feature likelihood functions.
+    /// </summary>
+    public class TargetPresenceLikelihoodFunctions
+    {
+        /// <summary>
+        /// The intensity only likelihood function.
+        /// </summary>
+        /// <param name="featureScores">
+        /// The feature scores.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public static double IntensityOnlyLikelihoodFunction(FeatureScoreHolder featureScores)
+        {
+            return featureScores.IntensityScore;
+        }
+
+        /// <summary>
+        /// The intensity independent likelihood function.
+        /// </summary>
+        /// <param name="featureScores">
+        /// The feature scores.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public static double IntensityIndependentLikelihoodFunction(FeatureScoreHolder featureScores)
+        {
+            return featureScores.IsotopicScore;
+        }
+
+        /// <summary>
+        /// The intensity dependent likelihood function.
+        /// </summary>
+        /// <param name="featureScores">
+        /// The feature scores.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public static double IntensityDependentLikelihoodFunction(FeatureScoreHolder featureScores)
+        {
+            return featureScores.IntensityScore + 2 * featureScores.IsotopicScore;
+        }
+    }
+}

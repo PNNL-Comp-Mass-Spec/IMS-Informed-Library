e242cce821bd15e2013b1daae42f742371b1c952
Jian <jian.ma@pnnl.gov>
2015-05-22 15:30:16 -0700

    Added missing files

---
+++ ImsInformed/Domain/DataAssociation/ObservationTransitionGraphRenderingEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImsInformed.Domain.DataAssociation
+{
+    using System.IO;
+
+    using QuickGraph.Graphviz;
+    using QuickGraph.Graphviz.Dot;
+
+    public class ObservationTransitionGraphRenderingEngine : IDotEngine
+    {
+        public string Run(GraphvizImageType imageType, string dot, string outputFileName)
+        {
+            // using (FileStream stream = new FileStream(outputFileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            // {
+            //     StreamWriter writer = new StreamWriter(stream);
+            //     writer.WriteLine(dot);
+            // }
+
+            string outputDir = "output";
+
+            File.WriteAllText(Path.Combine(outputDir, outputFileName + ".txt"), dot);
+
+            return outputFileName;
+        }
+    }
+}

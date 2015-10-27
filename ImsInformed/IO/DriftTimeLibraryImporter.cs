58527b0aefca417c6e6828f8d3ea12dd277c8484
Jian <jian.ma@pnnl.gov>
2015-10-27 14:36:51 -0700

    Added OSS license

--- ImsInformed/IO/DriftTimeLibraryImporter.cs
+++ ImsInformed/IO/DriftTimeLibraryImporter.cs
@@ -1,7 +1,13 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="DriftTimeLibraryImporter.cs" company="PNNL">
-//   Written for the Department of Energy (PNNL, Richland, WA)
-//   //   Copyright 2015, Battelle Memorial Institute.  All Rights Reserved.
+// The Software was produced by Battelle under Contract No. DE-AC05-76RL01830 with the Department of Energy.  
+// The U.S. Government is granted for itself and others acting on its behalf a nonexclusive, paid-up, 
+// irrevocable worldwide license in this data to reproduce, prepare derivative works, distribute copies to 
+// the public, perform publicly and display publicly, and to permit others to do so.  The specific term of
+// the license can be identified by inquiry made to Battelle or DOE.  NEITHER THE UNITED STATES NOR THE 
+// UNITED STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, MAKES ANY WARRANTY, EXPRESS OR IMPLIED, 
+// OR ASSUMES ANY LEGAL LIABILITY OR FOR THE ACCURACY, COMPLETENESS OR USEFULNESS OF ANY DATA, APPARATUS, 
+// PRODUCT OR PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
 // </copyright>
 // <summary>
 //   Defines the DriftTimeLibraryImporter type.
@@ -14,11 +20,7 @@ namespace ImsInformed.IO
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Windows;
 
-    using DeconTools.Backend.Utilities.Converters;
-
-    using ImsInformed.Domain;
     using ImsInformed.Targets;
 
     /// <summary>
